d7e1de0d797f3ea38c45dd0f11e221c519474965
Shadiya Egal <shadiya325@gmail.com>
2023-01-31 10:55:26 -0500

    commit tree aecda47fb54260589858f6e3b2bebdc77e6cb7a1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+# require "pry"
 
+class Book 
 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+        @author = nil
+        @page_count = 0
+        @genre = nil
+        @turn_page = turn_page
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    
+end 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+require "pry"
+
+class Shoe
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+    # def color
+    #     puts 'red'
+    # end
+end 
+
+
+
+
+
+binding.pry
